d05287c70d4ed2335188ffe1fe1b0e745de47fa8
mHotto <manuhotto@gmail.com>
2026-05-24 10:48:10 +0200

    mini casino

---
+++ dice.py
@@ -0,0 +1,37 @@
+import random
+salario=100
+caras = ["tails", "heads"]
+print("money initial:", salario, "if u wanna leave")
+while salario>0:
+ apuesta = int(input("how many want u bet?"))
+ if (apuesta > salario):
+  print("and the money?")
+  continue
+ tipo = input("dice or coin?").lower()
+ if tipo == "dice":
+  numero = input("pick a number (1,6)")
+  moneda=random.randint(1,6)
+  print(moneda)
+  if (numero == moneda):
+   salario= salario + apuesta*5
+   print("nice, currect money:", salario)
+  else:
+   salario -= int(apuesta)
+   print("u lose, current money:", salario)
+ elif salario<1:
+  break 
+ elif tipo == "coin":
+  numero = input("pick heads or tails: ")
+  moneda=random.choice(caras)
+  print("result: ", moneda)
+  if (numero == moneda):
+   salario= salario + apuesta*2
+   print("nice, currect money:", salario)
+  else:
+   salario -= int(apuesta)
+   print("u lose, current money:", salario)
+ elif salario<1:
+  break 
+  
+   
+  
